e3dd2eb5efc847f06deacb2104db55c6ec8e2520
Dejan Vasic <dejanvasic24@gmail.com>
2015-06-19 21:44:27 +1000

    Adding datepicker for the fixture date

--- client/app/fixture/newFixture/newFixture.directive.js
+++ client/app/fixture/newFixture/newFixture.directive.js
@@ -11,6 +11,8 @@ angular.module('meanteamApp').directive('mtNewFixture', function($log, $timeout)
 		link : function(scope, element, attrs){
 
 			scope.saving = false; // Flag used for button loader
+			scope.datePickerOpened = false;
+
 			scope.save = function(fixture){
 				
 				if(!scope.newFixtureForm.$valid || scope.newFixtureForm.$pristine) {
@@ -28,6 +30,25 @@ angular.module('meanteamApp').directive('mtNewFixture', function($log, $timeout)
 				var isValid = (formElement.$dirty && formElement.$valid) || formElement.$pristine;
 				return isValid;
 			};
+
+			scope.openDatePicker = function($event){
+				$event.preventDefault();
+    			$event.stopPropagation();
+				scope.datePickerOpened = !scope.datePickerOpened;
+			}
+
+			scope.isPastFixture = function(){
+				if(scope.fixture === undefined){
+					return true;
+				}
+
+				if(scope.fixture.date === undefined){
+					return true;
+				}
+
+				var now = new Date();
+				return scope.fixture.date < now;
+			}
 		}
 	}
 });
