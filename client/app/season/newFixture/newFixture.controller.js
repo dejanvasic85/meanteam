3868260c1e0f35aa02d5f98d06692b4075661e41
Dejan Vasic <dejanvasic24@gmail.com>
2015-11-01 09:31:52 +1100

    Getting the fixture form working on the new page

--- client/app/season/newFixture/newFixture.controller.js
+++ client/app/season/newFixture/newFixture.controller.js
@@ -2,7 +2,19 @@
 
 angular.module('meanteamApp')
   .controller('NewFixtureCtrl', function ($scope, $routeParams) {
-    $scope.vm = {
-      seasonName : $routeParams.seasonName
-    }
+
+    angular.extend($scope, {vm : {
+      seasonName : $routeParams.seasonName,
+      datePickerOpened : false,
+      fixture: null,
+      openDatePicker : function($event){
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.vm.datePickerOpened = !$scope.vm.datePickerOpened;
+      },
+      save : function(){
+        console.log($scope.vm.fixture)
+      }
+    }});
+
   });
